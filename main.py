54c58c51c4c063c23ce4b300a368a175c7a745fa
CatAgain <shine.u.chen@gmail.com>
2020-02-26 13:30:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='喵')
 
 @bot.event
 async def on_ready():
     print(" >>Bot is Online<< ")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
 
-bot.run('NjgyMDUyNTA2NTk5MDMwODE5.XlXZBQ.BQ0WB8R8ORYpKNB27xSBMDNGN4Y')
+bot.run("NjgyMDUyNTA2NTk5MDMwODE5.XlYA2w.60JMZPVm2gZRLC7gfqEAyyouuAQ", bot = True)
